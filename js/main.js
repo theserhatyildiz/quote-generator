718815fb66d16b1f77040781b78a280e99f8e3d2
Serhat Yildiz <yildizserhat@yahoo.com>
2023-09-18 14:14:52 +0300

    Finishes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+let quote = document.querySelector('#quote');
+let author = document.querySelector('#author');
+let btn = document.querySelector('#btn');
+
+const url = "https://api.quotable.io/random"
+
+let getQuote = () => {
+
+    fetch(url)
+    .then(data => data.json())
+    .then((item) => {
+        quote.innerText = item.content;
+        author.innerText = item.author;
+    });
+};
+
+window.addEventListener('load', getQuote)
+btn.addEventListener('click', getQuote)
